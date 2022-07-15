5a401cdf52077c01258f9602a1aa7b21848c4808
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-15 18:11:44 -0400

    add hero component, add hero styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import Main from "./components/Main";
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
       <Main />
     </div>
   );
---
+++ my-app/src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero--div">
+        <img src="../Group_77.png" className="hero--img" />
+      </div>
+      <div className="hero--text">
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
