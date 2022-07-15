a4613aa4e3baa9038c5006db7feba142c8def6d2
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-15 16:33:51 -0400

    airbnb nav component

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <img src="" alt="airbnb logo" className="nav--icon" />
+      <img src="../Airbnb_Logo.png" alt="airbnb logo" className="nav--icon" />
     </nav>
   );
 }
