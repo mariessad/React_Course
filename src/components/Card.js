323b4a489cad3212dbf0aa0f5fa742aee90e1f21
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-18 18:34:24 -0400

    add badges and badge CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../${props.img}`} className="card--img" alt="" />
       <div className="card--stats">
         <img src="../red_star.png" className="card--star" />
