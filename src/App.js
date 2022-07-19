d9f886c0ef427cc2e497054422bdcc825f2535e3
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-19 12:16:19 -0400

    pass entire card component, update dot notation in card componenet file

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ export default function App() {
     return (
       <Card
         key={card.id}
-        img={card.coverImg}
-        rating={card.stats.rating}
-        reviewCount={card.stats.reviewCount}
-        location={card.location}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
+        card={card}
+        // img={card.coverImg}
+        // rating={card.stats.rating}
+        // reviewCount={card.stats.reviewCount}
+        // location={card.location}
+        // title={card.title}
+        // price={card.price}
+        // openSpots={card.openSpots}
       />
     );
   });
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,24 +2,24 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.card.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.card.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`../${props.img}`} className="card--img" alt="" />
+      <img src={`../${props.card.coverImg}`} className="card--img" alt="" />
       <div className="card--stats">
         <img src="../red_star.png" className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.card.stats.rating}</span>
+        <span className="gray">({props.card.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.card.title}</p>
       <p className="card--price">
-        <span className="bold">${props.price}</span> / person
+        <span className="bold">${props.card.price}</span> / person
       </p>
     </div>
   );
