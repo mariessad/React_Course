fb454a11e71232ec80b88e517ede6c6bbbb17c59
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-20 15:05:11 -0400

    clear out files for new react practice project in react-3 branch

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,16 +1,5 @@
 import React from "react";
 
 export default function Main() {
-  return (
-    <main>
-      <h1 className="main--title">Learn About React!</h1>
-      <ul className="main--facts">
-        <li>Was first developed in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook /Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
-  );
+  return <main></main>;
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function Navbar() {
-  return (
-    <nav>
-      <img src="./React-icon.svg.png" alt="react logo" className="nav--icon" />
-      <h3 className="nav--logo_text">React Facts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
-    </nav>
-  );
+  return <nav></nav>;
 }
